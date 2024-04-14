8d2c4565b7f1680aa740cdaf1cd54602e3c61618
Guda <pranayguda39@csu.fullerton.edu>
2024-04-14 11:51:32 -0700

    Delete Item From Cart Functionality

--- src/Components/CartTile.jsx
+++ src/Components/CartTile.jsx
@@ -1,17 +1,27 @@
 import { useContext } from "react"
 import { FoodStoreContext } from "../store/FoodStoreContext"
+import deleteicon from '/delete-icon.svg'
 export default function CartTile({foodId,foodImage,foodTitle,foodPrice,foodQuantity}){
 
     const {state,actions} = useContext(FoodStoreContext);
     function handleChange(event){
         actions.updateQuantity(foodId,event.target.value);
     }
+
+    function handleRemove(){
+        actions.removeFromCart(foodId);
+    }
     return(
         <div className="cart-tile">
             <img src={foodImage} alt="" />
-            <p>{foodTitle}</p>
+            <div className="cart-item-title">
+                <p >{foodTitle}</p>
+                <button className="remove-cart" onClick={()=>handleRemove()} ><img src={deleteicon} alt="" srcset="" /></button>
+            </div>
             <input min={1} type="Number" onChange={(event)=>handleChange(event)} value={foodQuantity} />
-            <p>${foodPrice.toFixed(2)*foodQuantity}</p>
+            <p className="price">${foodPrice.toFixed(2)*foodQuantity}</p>
+    
         </div>
     )
 }
+
